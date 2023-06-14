8e2baa5e18fefa8717ac6251d1773bd53bc37343
chenchen <chenmudu@gmail.com>
2023-06-15 01:23:34 +0800

    fixes: add record and build test run.

---
+++ jfr-parser/chenmudu/jfr/parser/entities/chunks/Recording.java
@@ -0,0 +1,12 @@
+package jfr.parser.entities.chunks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recording {
+    /**
+     * A recording container one or more chunks.
+     */
+    private List<Chunk> chunks = new ArrayList<Chunk>();
+
+}
