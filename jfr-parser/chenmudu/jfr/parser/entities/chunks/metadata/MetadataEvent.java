018fc626aa692d2e8921389c718e7dcbdca4a4f0
chenchen <chenmudu@gmail.com>
2023-06-17 01:14:25 +0800

    feature: build header and part of  Medatada.

--- jfr-parser/chenmudu/jfr/parser/entities/chunks/metadata/MetadataEvent.java
+++ jfr-parser/chenmudu/jfr/parser/entities/chunks/metadata/MetadataEvent.java
@@ -1,10 +1,13 @@
 package jfr.parser.entities.chunks.metadata;
 
+import jfr.parser.leb128.Leb128CompressedByteBuffer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * chenmudu@gmail.com
@@ -27,11 +30,39 @@ public class MetadataEvent {
 
     private Long metaDataId;
 
+    private Integer stringCount;
+
     private Root root;
 
-
-    public MetadataEvent parseMetadataEvent(ByteBuffer buffer) {
-
-        return this;
+    private static MetadataEvent buildEmptyMetadataEvent() {
+        return new MetadataEvent();
     }
+
+    public static MetadataEvent buildMetadataEventProperties(ByteBuffer buffer) {
+        final MetadataEvent event = buildEmptyMetadataEvent();
+        Leb128CompressedByteBuffer bf = Leb128CompressedByteBuffer.getLeb128CompressedByteBuffer(buffer);
+        event.setSize(bf.getVarInt());
+        event.setEventType(bf.getVarLong());
+        event.setStart(bf.getVarLong());
+        event.setDuration(bf.getVarLong());
+        event.setMetaDataId(bf.getVarLong());
+        event.setStringCount(bf.getVarInt());
+        //set root.
+        final List<String> strs = new ArrayList<>(event.getStringCount());
+        for(int i = 0; i < event.getStringCount(); i++) {
+            strs.add(bf.getString());
+        }
+
+        //parse names;
+
+        //parse elements;
+
+        return event;
+    }
+
+    private Root getRootData() {
+        Root root = new Root();
+        return root;
+    }
+
 }
